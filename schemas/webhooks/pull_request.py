0f7c0805b286ec9019fcf100ad503fefef93351c
phamquiduong <phamquiduong@outlook.com>
2025-09-22 18:55:37 +0700

    Pull request closed handler

--- schemas/webhooks/pull_request.py
+++ schemas/webhooks/pull_request.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 from helpers.html import build_link
 from schemas.base import WebhookRequestBase
 from schemas.webhooks.github_user import GithubUser
@@ -7,16 +9,30 @@ class PullRequest(WebhookRequestBase):
     number: int
     title: str
     html_url: str
+    merged: bool
 
     def as_html(self) -> str:
         html_link = build_link(link=self.html_url, display=f'PR#{self.number}')
         return f'[{html_link} {self.title}]'
 
 
-class PullRequestOpenedHandler:
+class PullRequestHandlerBase(ABC):
     def __init__(self, pull_request: PullRequest, sender: GithubUser) -> None:
         self.pull_request = pull_request
         self.sender = sender
 
+    @abstractmethod
+    def build(self) -> str | None: ...
+
+
+class PullRequestOpenedHandler(PullRequestHandlerBase):
     def build(self) -> str | None:
-        return f'✨  {self.sender.as_html()}  opened  {self.pull_request.as_html}'
+        return f'✨  {self.sender.as_html()} opened {self.pull_request.as_html()}'
+
+
+class PullRequestClosedHandler(PullRequestHandlerBase):
+    def build(self) -> str | None:
+        if self.pull_request.merged is True:
+            return f'🔀  {self.sender.as_html()} merged {self.pull_request.as_html()}'
+
+        return f'❌  {self.sender.as_html()} closed {self.pull_request.as_html()}'
