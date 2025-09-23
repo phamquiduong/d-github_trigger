97dd1fa995b22044a7fcac417e7e4ab8d551d4b9
phamquiduong <phamquiduong@outlook.com>
2025-09-23 23:26:19 +0700

    Fix action not valid

--- constants/github.py
+++ constants/github.py
@@ -2,25 +2,37 @@ from enum import StrEnum
 
 
 class RequestActions(StrEnum):
-    OPENED = 'opened'
-    REOPENED = 'reopened'
-    CLOSED = 'closed'
-    SYNCHRONIZE = 'synchronize'
-    EDITED = 'edited'
-    ASSIGNED = 'assigned'
-    UNASSIGNED = 'unassigned'
-    LABELED = 'labeled'
-    UNLABELED = 'unlabeled'
-    LOCKED = 'locked'
-    UNLOCKED = 'unlocked'
-    REVIEW_REQUESTED = 'review_requested'
-    REVIEW_REQUEST_REMOVED = 'review_request_removed'
-    READY_FOR_REVIEW = 'ready_for_review'
-    CONVERTED_TO_DRAFT = 'converted_to_draft'
-    SUBMITTED = 'submitted'
-    DISMISSED = 'dismissed'
-    CREATED = 'created'
-    DELETED = 'deleted'
+    # pull_request
+    OPENED = "opened"
+    REOPENED = "reopened"
+    CLOSED = "closed"
+    SYNCHRONIZE = "synchronize"
+    EDITED = "edited"
+    ASSIGNED = "assigned"
+    UNASSIGNED = "unassigned"
+    LABELED = "labeled"
+    UNLABELED = "unlabeled"
+    LOCKED = "locked"
+    UNLOCKED = "unlocked"
+    REVIEW_REQUESTED = "review_requested"
+    REVIEW_REQUEST_REMOVED = "review_request_removed"
+    READY_FOR_REVIEW = "ready_for_review"
+    CONVERTED_TO_DRAFT = "converted_to_draft"
+    AUTO_MERGE_ENABLED = "auto_merge_enabled"
+    AUTO_MERGE_DISABLED = "auto_merge_disabled"
+
+    # pull_request_review
+    SUBMITTED = "submitted"
+    DISMISSED = "dismissed"
+
+    # review_comment + issue_comment
+    CREATED = "created"
+    DELETED = "deleted"
+
+    # workflow_run
+    REQUESTED = "requested"
+    COMPLETED = "completed"
+    IN_PROGRESS = "in_progress"
 
 
 class ReviewState(StrEnum):
